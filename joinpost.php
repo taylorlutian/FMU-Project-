e06edf67ac9be02d57340c81209796665cd97245
taylorlutian <74161806+taylorlutian@users.noreply.github.com>
2020-11-15 12:45:56 -0500

    Create joinpost.php

---
+++ joinpost.php
@@ -0,0 +1,18 @@
+<?php 
+include('connect.php'); 
+
+$name = $_POST['name'];
+$pw = $_POST['pw'];
+$email = $_POST['email'];
+$memo = $_POST['memo'];
+
+$query= "insert into the knack shack customer database(name, pw, email, memo)
+       values('$name,'$pw', '$email', '$memo') "; 
+
+mysqli_query($connection, $query); 
+mysqli_close($connection);
+?>
+
+<script> 
+location.href='Join.html'; 
+</script>
