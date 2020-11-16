fc4f47b16fd95c86e76b4674fb11f491a7bc19f8
taylorlutian <74161806+taylorlutian@users.noreply.github.com>
2020-11-15 21:13:23 -0500

    Update JoinPost.php

--- JoinPost.php
+++ JoinPost.php
@@ -7,12 +7,12 @@ $email = $_POST['email'];
 $memo = $_POST['memo'];
 
 $query= "insert into the knack shack customer database(name, pw, email, memo)
-       values('$name,'$pw', '$email', '$memo') "; 
+       values('$name','$pw', '$email', '$memo') "; 
 
 mysqli_query($connection, $query); 
 mysqli_close($connection);
 ?>
 
 <script> 
-location.href='Join.html'; 
+location.href='hompage.html'; 
 </script>
